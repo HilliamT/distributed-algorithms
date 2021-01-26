bc8115da6c3b22a691fe64cafe7116cb6a79e749
HilliamT <hilliamtung@gmail.com>
2021-01-26 23:23:19 +0000

    Implemented multi-hop network, an adaptation of the single-hop

---
+++ lab-two-flooding-algorithms/ex2-multi-hop/lib/peer.ex
@@ -0,0 +1,30 @@
+
+# distributed algorithms, n.dulay, 4 jan 21
+# basic flooding, v1
+
+defmodule Peer do
+
+def start do
+  IO.puts "Peer started at #{Helper.node_string()}"
+  receive do
+    { :register_peers, peers } ->
+      count_hellos(peers, 0)
+  end
+end
+
+def count_hellos(peers, hellos) do
+  receive do
+    { :hello } ->
+      if (hellos == 0) do
+        # Send hellos to peers
+        Enum.map(peers, fn peer -> send peer, { :hello } end)
+      end
+
+      # Increment hellos seen
+      count_hellos(peers, hellos + 1)
+    after
+      1000 -> IO.puts "Peer #{Helper.node_string()} Messages seen = #{hellos}"
+  end
+end
+
+end # module ------------------------

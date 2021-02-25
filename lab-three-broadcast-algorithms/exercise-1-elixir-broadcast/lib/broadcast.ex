788e8e0b9a6c60570ecf8336f2239fb1d1945b1c
HilliamT <hilliamtung@gmail.com>
2021-02-25 11:10:01 +0000

    Removed redundant :bind event

--- lab-three-broadcast-algorithms/exercise-1-elixir-broadcast/lib/broadcast.ex
+++ lab-three-broadcast-algorithms/exercise-1-elixir-broadcast/lib/broadcast.ex
@@ -19,7 +19,6 @@ defp start(:cluster_start, config) do
   peers = Enum.map(0..(config.n_peers - 1), fn x -> Node.spawn(:"peer#{x}_#{config.node_suffix}", Peer, :start, []) end)
 
   Enum.map(0..(config.n_peers - 1), fn i -> send Enum.at(peers, i), { :bind, peers, i } end)
-  for peer <- peers, do: send peer, { :bind, peers }
   for peer <- peers, do: send peer, { :broadcast, 1000, 3000 }
 end
 
